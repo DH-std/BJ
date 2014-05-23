41cb86176c5f43af5f9e7628c16e4040f23f0d35
dili <dili.hu@atomicobject.com>
2014-05-23 11:25:21 -0400

    Initial Commit

---
+++ BJ/AOBJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AOBJAppDelegate.h
+//  BJ
+//
+//  Created by Dili Hu on 5/23/14.
+//  Copyright (c) 2014 AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AOBJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BJ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BJ
+//
+//  Created by Dili Hu on 5/23/14.
+//  Copyright (c) 2014 AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AOBJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AOBJAppDelegate class]));
+    }
+}
